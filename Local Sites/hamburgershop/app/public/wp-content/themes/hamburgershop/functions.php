ec119e08e938a443512551db047800d3ba9fbf0c
akko11 <101851612+akko11@users.noreply.github.com>
2022-08-03 06:02:00 +0900

    定義されていない変数削除

--- Local Sites/hamburgershop/app/public/wp-content/themes/hamburgershop/functions.php
+++ Local Sites/hamburgershop/app/public/wp-content/themes/hamburgershop/functions.php
@@ -45,16 +45,4 @@ function readScript(){
 
 add_action('wp_enqueue_scripts', 'readScript');
 
-//アーカイブを有効にする
-function post_has_archive($args, $post_type)
-{
-if ( 'post' == $post_type ) {
-    $args['rewrite'] = true;
-    $args['has_archive'] = 'category'; // 任意のURL
-}
-return $args;
-}
-add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
-
-
 ?>
